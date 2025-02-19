8814f2041ae2968d813ef64431a3f5166898342e
Alex Warren <WarrenWorkshopMedia@gmail.com>
2025-02-19 14:39:10 -0500

    Update rolltohit.py

--- rolltohit.py
+++ rolltohit.py
@@ -4,3 +4,23 @@
 #This is a guided practice. Either follow with the video or your instructor will
 #go over this in class.
 #Video Link: https://youtu.be/89G5DN0-O3k
+
+import random
+
+random.seed()
+
+print("Enter the number of dice and the hit target: ")
+
+DiceCount = int(input("Number of dice: "))
+HitTarget = int(input("Enter the hit target: "))
+
+HitCount = 0
+DiceRoll = 0
+
+for i in range(DiceCount):
+    DiceRoll = random.randint(1,6)
+    #print(f"DiceRoll: {DiceRoll}")
+    if DiceRoll >= HitTarget:
+        HitCount += 1
+
+print(f"You rolled {DiceCount} dice with a hit target of {HitTarget} and hit {HitCount} times.")

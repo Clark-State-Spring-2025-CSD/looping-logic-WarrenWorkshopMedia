5f755ee039d0779b80b741fbe1379e2d1ab697d4
Alex Warren <WarrenWorkshopMedia@gmail.com>
2025-02-20 15:56:28 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -13,3 +13,13 @@
 #13
 #14
 #FizzBuzz
+
+for Number in range(1, 26):
+    if Number % 3 == 0 and Number % 5 == 0:
+        print("FizzBuzz")
+    elif Number % 3 == 0:
+        print("Fizz")
+    elif Number % 5 == 0:
+        print("Buzz")
+    else:
+        print(Number)
